3d1ad51b325b051644bff7c3bb1ab18af8ec6d52
Dohaof <3151456572@qq.com>
2026-05-27 21:43:34 +0800

    feat: 实现加减乘除四则运算功能

--- add.py
+++ add.py
@@ -1,2 +1,20 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    """加法运算"""
+    return a + b
+
+
+def subtract(a, b):
+    """减法运算"""
+    return a - b
+
+
+def multiply(a, b):
+    """乘法运算"""
+    return a * b
+
+
+def divide(a, b):
+    """除法运算，除零时抛出异常"""
+    if b == 0:
+        raise ZeroDivisionError("除数不能为零")
+    return a / b
